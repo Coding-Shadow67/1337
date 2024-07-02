db62c54510d6ac587c3c09390cacbba8113559ad
Abdellah Syani <asyani@e1r12p4.1337.ma>
2024-07-02 20:12:38 +0100

    akl

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -6,32 +6,51 @@
 /*   By: asyani <asyani@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 08:19:26 by asyani            #+#    #+#             */
-/*   Updated: 2024/07/01 14:30:26 by asyani           ###   ########.fr       */
+/*   Updated: 2024/07/02 10:14:46 by asyani           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	is_alpha(char c)
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+char	is_alpha(char c)
 {
 	if ((c >= 'a' && c <= 'z')
 		|| (c >= 'A' && c <= 'Z'))
 	{
-		ft_putchar(c);
+		return (c);
 	}
+	return (0);
 }
 
 char	*ft_strcapitalize(char *str)
 {
 	int	i;
-	int	c = 1;
+	int	capitalize;
 
+	capitalize = 1;
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (!(is_alpha(str[i])))
+		if (is_alpha(str[i]) || (str[i] >= 48 && str[i] <= 57))
 		{
-			str[i++];
+			if (capitalize && str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] -= 32;
+			}
+			else if (!capitalize && str[i] >= 'A' && str[i] <= 'Z')
+			{
+				str[i] += 32;
+			}
+			capitalize = 0;
 		}
-		if (is_alpha(str[i]) == c)
-		{
+		else
+			capitalize = 1;
+		i++;
 	}
-}	
+	return (str);
+}
--- c02/ex11/ft_putstr_non_printable.c
+++ c02/ex11/ft_putstr_non_printable.c
@@ -6,17 +6,46 @@
 /*   By: asyani <asyani@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 19:51:39 by asyani            #+#    #+#             */
-/*   Updated: 2024/07/01 19:56:04 by asyani           ###   ########.fr       */
+/*   Updated: 2024/07/02 16:14:40 by asyani           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_putstr_non_printable(char *str)
+void	ft_putchar(char c)
 {
-	int	i;
-	int	j;
+	write(1, &c, 1);
+}
+char    is_printable(char c)
+{
+    if (c > 32 && c <= 126)
+    {
+        return (c);
+    }
+    return (0);
+}
 
-	i = 0;
+void    to_hex(int n)
+{
 
-	while (str[i] != '\0')
-	{
-		if ( 
+}
+
+void ft_putstr_non_printable(char *str)
+{
+    int i;
+    int count;
+
+    count = 0;
+    i = 0;
+    while (str[i] != '\0')
+    {
+        if (is_printable(str[i]))
+        {
+            ft_putchar(str[i]);
+        }
+        else if (!is_printable(str[i]))
+        {
+			if (str[i] >= 10)
+			{
+        }
+    }
+}
+
