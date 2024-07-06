002a97196b82873a8751a34ff607f8a274ca9e38
Abdellah Syani <asyani@e1r12p4.1337.ma>
2024-07-06 16:14:59 +0100

    m

--- c03/ex05/ft_strlcat.c
+++ c03/ex05/ft_strlcat.c
@@ -6,26 +6,40 @@
 /*   By: asyani <asyani@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 17:46:47 by asyani            #+#    #+#             */
-/*   Updated: 2024/07/04 17:58:28 by asyani           ###   ########.fr       */
+/*   Updated: 2024/07/06 15:45:21 by asyani           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
 
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
 	unsigned int	i;
-	unsigned int	j;
+	unsigned int	dest_len;
+	unsigned int	src_len;
 
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
 	i = 0;
-	j = 0;
-	while (dest[i] != '\0')
+	if (size <= dest_len)
 	{
+		return (src_len + size);
+	}
+	while (src[i] && dest_len + i < size - 1)
+	{
+		dest[dest_len + i] = src[i];
 		i++;
 	}
-	while (j < size && src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	return (j);
+	dest[dest_len + i] = '\0';
+	return (dest_len + src_len);
 }
