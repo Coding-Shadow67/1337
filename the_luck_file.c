4993111dbc54dfb17f0e6df3e8ba7706b307a4c6
Abdellah Syani <asyani@e1r12p3.1337.ma>
2024-07-19 07:25:56 +0100

    inshaallah

---
+++ the_luck_file.c
@@ -0,0 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   the_luck_file.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asyani <asyani@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/19 07:20:35 by asyani            #+#    #+#             */
+/*   Updated: 2024/07/19 07:24:41 by asyani           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+this file is for wishing me the best of luck in the exam final in 1337 and I will see this message after four month when I be a student in 1337 inshaallah and i will remember the pool days
+good luck to me.
+alahoma la sahla ila ma ja3altaho sahlan wa anta taj3alo lhazana ida chi2ta sahlan
